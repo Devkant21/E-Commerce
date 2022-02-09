3c3fe3d1cb7704b6f49605b8092d9416b2d11869
Devkant <devkant21@gmail.com>
2022-02-09 15:47:19 +0530

    navbar

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCo7VeEwd5e-9sLN_YlgAux5bXzUXQ4ZQU",
+    authDomain: "hooks-c7fd0.firebaseapp.com",
+    projectId: "hooks-c7fd0",
+    storageBucket: "hooks-c7fd0.appspot.com",
+    messagingSenderId: "1084444210893",
+    appId: "1:1084444210893:web:6894ccb5a9149951ee9b84",
+    measurementId: "G-J5K3XGRRWP"
+  };
